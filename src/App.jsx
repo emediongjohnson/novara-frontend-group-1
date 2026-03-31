58363b63cb70f1f66fd0cd9ad507e384f3539c7c
emediongjohnson <emediongjohnson@gmail.com>
2026-03-31 12:31:46 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import InfoSection from './components/InfoSection/InfoSection';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+import Planets from './components/Planets/Planets';
+import PlanetTable from './components/PlanetTable/PlanetTable';
+import ContactForm from './components/ContactForm/ContactForm';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <InfoSection />
+      <VideoPlayer />
+      <Planets />
+      <PlanetTable />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
