aa802c142366239c02f104155b9835207ab8d836
emediongjohnson <emediongjohnson@gmail.com>
2026-04-03 17:52:48 +0100

    feat: implement smooth scroll navigation from Hero to Planets
    Added functionality to scroll to planets section when the Hero component is clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import InfoSection from './components/InfoSection/InfoSection';
-import VideoPlayer from './components/VideoPlayer/VideoPlayer';
-import Planets from './components/Planets/Planets';
-import PlanetTable from './components/PlanetTable/PlanetTable';
-import ContactForm from './components/ContactForm/ContactForm';
-import Footer from './components/Footer/Footer';
+import { useRef } from "react";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import InfoSection from "./components/InfoSection/InfoSection";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+import Planets from "./components/Planets/Planets";
+import PlanetTable from "./components/PlanetTable/PlanetTable";
+import ContactForm from "./components/ContactForm/ContactForm";
+import Footer from "./components/Footer/Footer";
 
 function App() {
+  const planetsRef = useRef(null);
+
+  const scrollToPlanets = () => {
+    console.log("Scrolling..."); // 🔥 debug
+    planetsRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
       <Header />
-      <Hero />
+
+      {/* PASS FUNCTION */}
+      <Hero onExploreClick={scrollToPlanets} />
+
       <InfoSection />
       <VideoPlayer />
-      <Planets />
+
+      {/* ATTACH REF */}
+      <Planets refProp={planetsRef} />
+
       <PlanetTable />
       <ContactForm />
       <Footer />
