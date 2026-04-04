3264da439a99ded99c9f998117345b808330904b
emediongjohnson <emediongjohnson@gmail.com>
2026-04-04 21:12:23 +0100

    Add InfoSection component with introductory content

---
+++ src/components/InfoSection/InfoSection.jsx
@@ -0,0 +1,22 @@
+import "./InfoSection.css";
+
+function InfoSection() {
+  return (
+    <section className="info">
+      <div className="info-container">
+        
+        <h2>How Planetary Data Helps Us Understand Space</h2>
+
+        <p>
+          Planetary science goes beyond images. Comparing{" "}
+          <span>mass</span>, <span>diameter</span>, <span>gravity</span>, and{" "}
+          <span>density</span>, we gain insight into how planets form, behave,
+          and interact within the solar system.
+        </p>
+
+      </div>
+    </section>
+  );
+}
+
+export default InfoSection;
